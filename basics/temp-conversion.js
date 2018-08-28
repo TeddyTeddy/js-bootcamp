a7d17a5e0d1e1f55c73f59f150fc84d300f86f7f
HC <hakan.cuzdan@yandex.com>
2018-08-28 14:39:36 +0300

    Section 3 : Lecture 13 : Build a temperature converter

---
+++ basics/temp-conversion.js
@@ -0,0 +1,10 @@
+let fahrenheit = 32 // 0C - 273.15k
+// calculate celsius and store in celsius variable
+let celsius = (fahrenheit - 32) * 5/9
+
+// calculate kelvin and store in kelvin variable
+let kelvin = (fahrenheit + 459.67) * 5/9
+
+console.log('celsius: ', celsius)
+console.log('fahreneit: ', fahrenheit)
+console.log('kelvin: ', kelvin)
