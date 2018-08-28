b0633c9728c19c384a299925f37be848fec2be30
HC <hakan.cuzdan@yandex.com>
2018-08-28 12:39:22 +0300

    Section 3: Lecture 10: Strings & Variables

---
+++ basics/strings.js
@@ -0,0 +1,11 @@
+let firstName = 'Andew'
+let lastName = 'Mead'
+let fullName = firstName + ' ' + lastName
+
+console.log(fullName)
+
+let city = 'Helsinki'
+let county = 'Finland'
+let location = city + ', ' + county
+
+console.log(location)
