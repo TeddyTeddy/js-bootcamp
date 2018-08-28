515ba45168ab4e69584288209a089819d48c13db
HC <hakan.cuzdan@yandex.com>
2018-08-28 13:06:24 +0300

    Section 3: Lecture 11 : Numbers

---
+++ basics/numbers.js
@@ -0,0 +1,18 @@
+// operator precedence in js:
+// MDAS: My Dear And Sally
+// Multiplication, Division, Addition and Substraction
+let x = 11
+let num = (x + 1) * 2
+console.log(num)
+
+let age = 40
+let dogYears = (age + 1) / 7
+
+console.log(dogYears)
+
+// Challange Area
+
+let studentScore = 18
+let maxScore = 20
+let percent = (studentScore / maxScore) * 100 // MDAS
+console.log(percent)
