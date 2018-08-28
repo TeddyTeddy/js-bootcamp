53b937b776d67f07c964e45b84a3d2f321cbbe9c
HC <hakan.cuzdan@yandex.com>
2018-08-28 15:12:28 +0300

    Section 3 : Lecture 14 : Booleans and Comparison operators

---
+++ basics/boolean.js
@@ -0,0 +1,19 @@
+// === - equality operator
+// !=== - not equality operator
+// < - less than operator
+// > - greater than operator
+// <= - less then or equal to
+// >= - greater than or equal to
+let temp = 32
+let isFreezing = (temp <= 32)
+console.log(isFreezing)
+
+// CHALLENGE
+// under <= 7 & over >= 65 gets discount
+let age = 70
+let isChild = age <= 7
+let isSenior = age >= 65
+console.log('age', age)
+console.log('isChild', isChild)
+console.log('isSenior', isSenior)
+
