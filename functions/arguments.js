7cd8bf768bb28bd0c57ad0f0a6971fcf333fad92
HC <hakan.cuzdan@yandex.com>
2018-08-30 17:00:45 +0300

    Section 4 : Lecture 25 : Template Strings

--- functions/arguments.js
+++ functions/arguments.js
@@ -8,7 +8,8 @@ console.log(result)
 
 // Default arguments
 let getScoreText = function(name = 'Anonymous', score = 0) {
-    return 'Name: ' + name + ' - Score: ' + score
+   return `Name: ${name} - Score: ${score}`
+    // return 'Name: ' + name + ' - Score: ' + score
 }
 
 let scoreText = getScoreText('Adrew')
@@ -18,12 +19,15 @@ scoreText = getScoreText(undefined, 99)
 console.log(scoreText)
 
 // CHALLANGE AREA
+// A 25% tip on $40 would be $10
 let getTip = function(total, tipPercent = .2) {
-    return total * tipPercent
+    return `A ${tipPercent * 100}% tip on $${total} would be $${total * tipPercent}`
 }
 
 console.log('getTip(100): ', getTip(100))
 console.log('getTip(100, 0.1): ', getTip(100, 0.1))
 console.log('getTip(100, 0.5): ', getTip(100, 0.5))
 
-
+let name = 'Jen'
+let age = 9
+console.log(`Hey, my name is ${name}! I am ${age} years old`)
