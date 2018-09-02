7d6567d4b10fd7b0d27b3aebb0917ec675a2f2ab
HC <hakan.cuzdan@yandex.com>
2018-09-02 18:13:39 +0300

    Section 5 : Lecture 36 : Variables with VAR

--- objects/var-2.js
+++ objects/var-2.js
@@ -14,3 +14,11 @@ var name  = 'Hakan' // when we declare a variable via VAR keyword
 surname = 'Cuzdan'
 console.log(surname) // Cuzdan
 var surname
+
+// To summarize:
+// If we try to access a VAR variable before it gets declared
+// we get UNDEFINED
+// If we try to access a LET or CONST variable before it gets
+// declared, we get a reference error, that is the expected
+// behaviour. We would check our code, why a variable is 
+// used in the code before it is declared and fix the problem
