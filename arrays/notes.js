4970f97b470944542de384c1af9b1840a78bcb7d
HC <hakan.cuzdan@yandex.com>
2018-09-03 14:55:19 +0300

    Section 6 : Lecture 39 : Manipulating Arrays with Methods

--- arrays/notes.js
+++ arrays/notes.js
@@ -1,7 +1,33 @@
 const notes = ['Note 1', 'Note 2', 'Note 3']
 
-console.log(notes.length)
-console.log(notes[0]) // 'Note 1'
-console.log(notes[2]) // 'Note 3'
-console.log(notes[34]) // undefined
-console.log(notes[notes.length-1]) // 'Note 3'
+// console.log(notes.length)
+// console.log(notes[0]) // 'Note 1'
+// console.log(notes[2]) // 'Note 3'
+// console.log(notes[34]) // undefined
+// console.log(notes[notes.length-1]) // 'Note 3'
+
+// notes.push('My new note') // adds a new item to the end
+// console.log(notes.length)
+// console.log(notes) // [ 'Note 1', 'Note 2', 'Note 3', 'My new note' ]
+
+// console.log(notes.pop())  // removes an item from the end
+// console.log(notes) // [ 'Note 1', 'Note 2', 'Note 3' ]
+
+// console.log(notes.shift()) // removes the first item from the array and returns it
+// console.log(notes) // [ 'Note 2', 'Note 3' ]
+
+// notes.unshift('My first note')
+// console.log(notes) // [ 'My first note', 'Note 2', 'Note 3' ]
+
+
+console.log(notes.splice(1, 1)) // remove Note 2 and return ['Note 2']
+console.log(notes.splice(1, 0, 'This is the new second item')) // []
+console.log(notes)
+
+// replace note 3
+notes.splice(2,1, 'This is the new Note 3')
+console.log(notes)
+
+notes[1] = 'Note 2'
+notes[2] = 'Note 3'
+console.log(notes)
--- arrays/todo.js
+++ arrays/todo.js
@@ -2,3 +2,14 @@ const todos = ['Order cat food', 'Clean kitchen', 'Buy food', 'Do work', 'Exerci
 console.log(`You have ${todos.length} todos`)
 console.log(`Todo: ${todos[0]}`)  // 'Todo 1'
 console.log(`Todo: ${todos[todos.length - 2]}`) // 'Todo 4'
+
+
+// Delete the 3.rd item
+todos.splice(2, 1)
+// Add a new item onto the end
+todos.push('Buy coffee')
+// Remove the first item from the list
+todos.shift()
+
+console.log(`You have ${todos.length} todos`)
+console.log(todos)
