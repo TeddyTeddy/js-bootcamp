6a9827a4dc74418dac6dcd2ecf07ca4548b96cf1
HC <hakan.cuzdan@yandex.com>
2018-09-03 13:51:07 +0300

    Section 5 : Lecture 38 : Array Basics

---
+++ arrays/notes.js
@@ -0,0 +1,7 @@
+const notes = ['Note 1', 'Note 2', 'Note 3']
+
+console.log(notes.length)
+console.log(notes[0]) // 'Note 1'
+console.log(notes[2]) // 'Note 3'
+console.log(notes[34]) // undefined
+console.log(notes[notes.length-1]) // 'Note 3'
