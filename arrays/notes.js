a32390ec06f45b92123d538947d6090c0677a12c
HC <hakan.cuzdan@yandex.com>
2018-09-04 14:35:46 +0300

    Section 6 : Lecture 42 : Searching Arrays : .forEach(), .indexOf() .findIndex()

--- arrays/notes.js
+++ arrays/notes.js
@@ -1,4 +1,28 @@
-const notes = ['Note 1', 'Note 2', 'Note 3']
+const notes = [{},{
+    title: 'My next trip',
+    body: 'I would like to go to Spain'
+}, {
+    title: 'Habbits to work on',
+    body: 'Exercise. Eating a bit better.'
+}, {
+    title: 'Office modification',
+    body: 'Get a new seat'
+}]
+
+console.log(notes.length)
+console.log(notes)
+console.log('notes.indexOf({})', notes.indexOf({})) // -1, coz indexOf uses === operator
+console.log('notes.indexOf(notes[0])', notes.indexOf(notes[0])) // 0
+console.log('{} === {}', {} === {}) // false
+let someObject = {}
+let otherObject = someObject
+console.log('someObject === otherObject', someObject === otherObject) // true
+
+const index = notes.findIndex(function(note, index){
+    console.log(note)
+    return note.title === 'Habbits to work on'
+})
+console.log(index)
 
 // console.log(notes.length)
 // console.log(notes[0]) // 'Note 1'
@@ -20,40 +44,44 @@ const notes = ['Note 1', 'Note 2', 'Note 3']
 // console.log(notes) // [ 'My first note', 'Note 2', 'Note 3' ]
 
 
-console.log(notes.splice(1, 1)) // remove Note 2 and return ['Note 2']
-console.log(notes.splice(1, 0, 'This is the new second item')) // []
-console.log(notes)
+// console.log(notes.splice(1, 1)) // remove Note 2 and return ['Note 2']
+// console.log(notes.splice(1, 0, 'This is the new second item')) // []
+// console.log(notes)
 
-// replace note 3
-notes.splice(2,1, 'This is the new Note 3')
-console.log(notes)
+// // replace note 3
+// notes.splice(2,1, 'This is the new Note 3')
+// console.log(notes)
 
-notes[1] = 'Note 2'
-notes[2] = 'Note 3'
-console.log(notes)
+// notes[1] = 'Note 2'
+// notes[2] = 'Note 3'
+// console.log(notes)
 
 
-notes.forEach(function( item, index ) {
-    console.log(index, item)
-})
+// notes.forEach(function( item, index ) {
+//     console.log(index, item)
+// })
 
-// Counting 1, 2, 3..
-// configuration details goes inside for paranthesis
-// the first is initializer
-// the second is a condition
-// the third is the final expression
-for(let count = 0; count < 3; ++count) { // count var is scoped locally!
-    console.log(`Counting ${count + 1}`)
-}
+// // Counting 1, 2, 3..
+// // configuration details goes inside for paranthesis
+// // the first is initializer
+// // the second is a condition
+// // the third is the final expression
+// for(let count = 0; count < 3; ++count) { // count var is scoped locally!
+//     console.log(`Counting ${count + 1}`)
+// }
 
-for(let count = 2; count >= 0; --count) { // count var is scoped locally!
-    console.log(`Counting ${count + 1}`)
-}
+// for(let count = 2; count >= 0; --count) { // count var is scoped locally!
+//     console.log(`Counting ${count + 1}`)
+// }
 
-for(let count = 0; count < notes.length; ++count) {
-    console.log(notes[count])
-}
+// for(let count = 0; count < notes.length; ++count) {
+//     console.log(notes[count])
+// }
 
-for(let count = notes.length - 1; count >= 0; --count) {
-    console.log(notes[count])
-}
+// for(let count = notes.length - 1; count >= 0; --count) {
+//     console.log(notes[count])
+// }
+
+// console.log(notes.indexOf('Note 2')) // 1
+// console.log(notes.indexOf('note 2')) // -1
+
