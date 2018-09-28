d525d296afba5e6cd836f591ddbfcf0184de1ec7
HC <hakan.cuzdan@yandex.com>
2018-09-28 13:28:10 +0300

    Section 7 : Lecture 52 : Todo app : Adding Elements via the DOM

--- notes-app/notes-app.js
+++ notes-app/notes-app.js
@@ -6,6 +6,17 @@
 // const p = document.querySelector('p')
 // p.remove()
 
+const notes = [{
+    title: 'My next trip',
+    body: 'I would like to go to Spain'
+}, {
+    title: 'Habbits to work on',
+    body: 'Exercise. Eating a bit better.'
+}, {
+    title: 'Office modification',
+    body: 'Get a new seat'
+}]
+
 // query all and remove
 const ps = document.querySelectorAll('p')
 ps.forEach(function(p) {
@@ -13,3 +24,8 @@ ps.forEach(function(p) {
     //console.log(p.textContent)
     //p.remove()
 })
+
+// add a new element
+const newParagraph = document.createElement('p')
+newParagraph.textContent = 'This is a new element from JS'
+document.querySelector('body').appendChild(newParagraph)
--- todo-app/todo-app.js
+++ todo-app/todo-app.js
@@ -1,6 +1,34 @@
-const ps = document.querySelectorAll('p')
-ps.forEach(function(p) {
-    if(p.textContent.includes('the')) {
-        p.remove()
+const todos = [{
+    text: 'Order cat food',
+    completed: true
+}, {
+    text: 'Clean kitchen',
+    completed: false
+}, {
+    text: 'Buy food',
+    completed: true
+}, {
+    text: 'Do work',
+    completed: false
+}, {
+    text: 'Exercise',
+    completed: true
+}]
+
+// You have 2 todos left
+let count = 0
+todos.forEach(function(todo) {
+    if(!todo.completed) {
+        ++count
     }
+})
+const summary = document.createElement('h2')
+summary.textContent = `You have ${count} todo(s) left`
+document.querySelector('body').appendChild(summary)
+
+// Add a p for each todo above (use text value)
+todos.forEach(function(todo) {
+    const p = document.createElement('p')
+    p.textContent = todo.text
+    document.querySelector('body').appendChild(p)
 })
