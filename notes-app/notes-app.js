bfb27a4be576a4256a0c0bafaba4dd5a2e7e4abf
HC <hakan.cuzdan@yandex.com>
2018-09-28 11:35:39 +0300

    Section 7 : Lecture 50: DOM Manipulation

--- notes-app/notes-app.js
+++ notes-app/notes-app.js
@@ -1,1 +1,15 @@
-console.log('This is from notes-app.js')
+// DOM - Document Object Model
+// Document: The HTML document
+// Object: JS object modeling the HTML document
+
+// query and remove
+// const p = document.querySelector('p')
+// p.remove()
+
+// query all and remove
+const ps = document.querySelectorAll('p')
+ps.forEach(function(p) {
+    p.textContent = '*****'
+    //console.log(p.textContent)
+    //p.remove()
+})
