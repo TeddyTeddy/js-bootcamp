3b7d146949b4f5b9c556631989b2ebba63a989e6
HC <hakan.cuzdan@yandex.com>
2018-09-29 16:01:16 +0300

    Section 7 : Lecture 57 : Todo app filter challange

--- todo-app/todo-app.js
+++ todo-app/todo-app.js
@@ -15,30 +15,58 @@ const todos = [{
     completed: true
 }]
 
-// You have 2 todos left
-let count = 0
-todos.forEach(function(todo) {
-    if(!todo.completed) {
-        ++count
-    }
-})
-const summary = document.createElement('h2')
-summary.textContent = `You have ${count} todo(s) left`
-document.querySelector('body').appendChild(summary)
+// 1. create a div containing all todos
+// 2. Render inside the div, not in the body
+// 3. create renderTodos function, taking in notes and filters as args
+// 4. call renderTodos
+// 5. at input event, update the filter and renderTodos with the new filter
 
-// Add a p for each todo above (use text value)
-todos.forEach(function(todo) {
-    const p = document.createElement('p')
-    p.textContent = todo.text
-    document.querySelector('body').appendChild(p)
-})
+const filters = {
+    searchText: ''
+}
+
+const renderTodos = function(todos, filters) {
+    // get filtered todos, case insensitive filtering
+    const filteredTodos = todos.filter(function(todo) {
+        return todo.text.toLowerCase().includes(filters.searchText.toLowerCase())
+    })
+
+    // render 'You have 2 todos left'
+    let count = 0
+    filteredTodos.forEach(function(todo) {
+        if(!todo.completed) {
+            ++count
+        }
+    })
+
+    // clear old summary
+    document.querySelector('#todos').innerHTML = ''
+
+    const summary = document.createElement('h2')
+    summary.textContent = `You have ${count} todo(s) left`   
+    document.querySelector('#todos').appendChild(summary)
+
+    // Render filtered todos
+    filteredTodos.forEach(function(todo) {
+        const p = document.createElement('p')
+        p.textContent = todo.text
+        document.querySelector('#todos').appendChild(p)
+    })    
+}
+
+renderTodos(todos, filters)
 
 // listen for new todo creation
 document.querySelector('#add-todo').addEventListener('click', function(e) {
     e.target.textContent = 'button clicked'
 })
 
-// listen for keystorekes in #new-todo-text input element
 document.querySelector('#new-todo-text').addEventListener('input', function(e) {
     console.log(e.target.value)
+})
+
+// listen for keystorekes in #search-tex input element
+document.querySelector('#search-text').addEventListener('input', function(e) {
+    filters.searchText = e.target.value
+    renderTodos(todos, filters)
 })
