8eeee4f5f0b55b3a627d3d6e813c19de8514e215
HC <hakan.cuzdan@yandex.com>
2018-10-17 11:54:34 +0300

    Section 8 : Lecture 78 : Notes App : My improvement : DOMContentLoaded event is utilized in edit.html and in index.html

--- notes-app/notes-app.js
+++ notes-app/notes-app.js
@@ -1,56 +1,62 @@
-const filterByDOM = document.querySelector('#filter-by')
-let notes = getSavedNotes() // from local storage
-const filters = {
-    searchText: '',
-    sortBy: filterByDOM.value  // byEdited at default
+document.addEventListener('DOMContentLoaded', runNotesApp)
+
+function runNotesApp() {
+
+    const filterByDOM = document.querySelector('#filter-by')
+    let notes = getSavedNotes() // from local storage
+    const filters = {
+        searchText: '',
+        sortBy: filterByDOM.value  // byEdited at default
+    }
+    
+    renderNotes(notes, filters)
+    
+    // creating a new note functionality
+    document.querySelector('#create-note').addEventListener('click', function(e) {
+        const uuid = uuidv4()
+        const now = moment().valueOf()
+        notes.push({
+            id: uuid,
+            title: '',
+            body: '',
+            createdAt: now,
+            updatedAt: now
+        })
+        saveNotes(notes) // to local storage
+        location.assign(`/edit.html#${uuid}`)
+    })
+    
+    // search text filtering functionality
+    document.querySelector('#search-text').addEventListener('input', function(e) {
+        filters.searchText = e.target.value
+        renderNotes(notes, filters)
+    })
+    
+    document.querySelector('#filter-by').addEventListener('change', function(e) {
+        // update filters.sortBy property with the value set in the #filter-by
+        filters.sortBy = e.target.value
+        renderNotes(notes, filters)
+    })
+    
+    // remove note event handler utilizing the event bubling
+    document.querySelector('#notes').addEventListener('click', function(e) {
+        if(e.target.tagName === 'BUTTON') {
+            // grab the parent div's id as uuid
+            const targetID = e.target.parentElement.getAttribute('id')
+            // Delete from the notes array the note with the uuid
+            removeNote(notes, targetID)
+            saveNotes(notes) // to local storage
+            renderNotes(notes, filters)
+        }
+    })
+    
+    // local storage changed event
+    window.addEventListener('storage', function(e) {
+        if((e.key === 'notes') || (localStorage.getItem('notes') === null)) { 
+            // take action only if notes item in LocalStorage got changed/deleted
+            notes = getSavedNotes() // from local storage
+            renderNotes(notes, filters)
+        }
+    })
 }
 
-renderNotes(notes, filters)
-
-// creating a new note functionality
-document.querySelector('#create-note').addEventListener('click', function(e) {
-    const uuid = uuidv4()
-    const now = moment().valueOf()
-    notes.push({
-        id: uuid,
-        title: '',
-        body: '',
-        createdAt: now,
-        updatedAt: now
-    })
-    saveNotes(notes) // to local storage
-    location.assign(`/edit.html#${uuid}`)
-})
-
-// search text filtering functionality
-document.querySelector('#search-text').addEventListener('input', function(e) {
-    filters.searchText = e.target.value
-    renderNotes(notes, filters)
-})
-
-document.querySelector('#filter-by').addEventListener('change', function(e) {
-    // update filters.sortBy property with the value set in the #filter-by
-    filters.sortBy = e.target.value
-    renderNotes(notes, filters)
-})
-
-// remove note event handler utilizing the event bubling
-document.querySelector('#notes').addEventListener('click', function(e) {
-    if(e.target.tagName === 'BUTTON') {
-        // grab the parent div's id as uuid
-        const targetID = e.target.parentElement.getAttribute('id')
-        // Delete from the notes array the note with the uuid
-        removeNote(notes, targetID)
-        saveNotes(notes) // to local storage
-        renderNotes(notes, filters)
-    }
-})
-
-// local storage changed event
-window.addEventListener('storage', function(e) {
-    if((e.key === 'notes') || (localStorage.getItem('notes') === null)) { 
-        // take action only if notes item in LocalStorage got changed/deleted
-        notes = getSavedNotes() // from local storage
-        renderNotes(notes, filters)
-    }
-})
--- notes-app/notes-edit.js
+++ notes-app/notes-edit.js
@@ -1,66 +1,70 @@
-const noteTitleDOM = document.querySelector('#note-title')
-const noteBodyDOM = document.querySelector('#note-body')
-const removeButtonDOM = document.querySelector('#remove-note')
-const lastEditedDOM = document.querySelector('#last-edited')
+document.addEventListener('DOMContentLoaded', runNotesEdit)
 
-let noteId, notes, note
-
-const init = function() {
-    noteId = location.hash.substring(1)
-    notes = getSavedNotes()
-    note = notes.find(function(note) {
-        return note.id === noteId
-    })
+function runNotesEdit() {
+    const noteTitleDOM = document.querySelector('#note-title')
+    const noteBodyDOM = document.querySelector('#note-body')
+    const removeButtonDOM = document.querySelector('#remove-note')
+    const lastEditedDOM = document.querySelector('#last-edited')
     
-    // if note is not found, redirect to main page
-    if(!note) {
-        location.assign('/index.html')
+    let noteId, notes, note
+    
+    const init = function() {
+        noteId = location.hash.substring(1)
+        notes = getSavedNotes()
+        note = notes.find(function(note) {
+            return note.id === noteId
+        })
+        
+        // if note is not found, redirect to main page
+        if(!note) {
+            location.assign('/index.html')
+        }
+        
+        // note is found, show the note's contents in UI
+        noteTitleDOM.value = note.title
+        noteBodyDOM.value = note.body
+        // update the last edited text in ui
+        lastEditedDOM.textContent = generatedLastEditedText(note.updatedAt)
     }
     
-    // note is found, show the note's contents in UI
-    noteTitleDOM.value = note.title
-    noteBodyDOM.value = note.body
-    // update the last edited text in ui
-    lastEditedDOM.textContent = generatedLastEditedText(note.updatedAt)
+    init()
+    
+    // updating the note title functionality
+    noteTitleDOM.addEventListener('input', function(e) {
+        // update the note.title with the value from ui
+        note.title = e.target.value // note obj is a reference in notes array!
+        // update the updatedAt property for the note
+        note.updatedAt = moment().valueOf()
+        // update the last edited text in ui    
+        lastEditedDOM.textContent = generatedLastEditedText(note.updatedAt)
+        saveNotes(notes) // to local storage
+    })
+    
+    // updating the note body functionality
+    noteBodyDOM.addEventListener('input', function(e) {
+        // update the note body with the value from ui
+        note.body = e.target.value // note obj is a reference in notes array!
+        // update the updatedAt property for the note
+        note.updatedAt = moment().valueOf()
+        // update the last edited text in ui
+        lastEditedDOM.textContent = generatedLastEditedText(note.updatedAt)
+        saveNotes(notes) // to local storage
+    })
+    
+    // removing the note functionality
+    removeButtonDOM.addEventListener('click', function(e) {
+        // remove the note from notes array
+        removeNote(notes, note.id)
+        saveNotes(notes) // to local storage
+        // redirect to home page
+        location.assign('/index.html')
+    })
+    
+    // listem fpr changes in local storage
+    window.addEventListener('storage', function(e) {
+        if((e.key === 'notes') || (localStorage.getItem('notes') === null)) { 
+            // act only if notes item is modified/deleted in localStorage
+            init()
+        } 
+    })    
 }
-
-init()
-
-// updating the note title functionality
-noteTitleDOM.addEventListener('input', function(e) {
-    // update the note.title with the value from ui
-    note.title = e.target.value // note obj is a reference in notes array!
-    // update the updatedAt property for the note
-    note.updatedAt = moment().valueOf()
-    // update the last edited text in ui    
-    lastEditedDOM.textContent = generatedLastEditedText(note.updatedAt)
-    saveNotes(notes) // to local storage
-})
-
-// updating the note body functionality
-noteBodyDOM.addEventListener('input', function(e) {
-    // update the note body with the value from ui
-    note.body = e.target.value // note obj is a reference in notes array!
-    // update the updatedAt property for the note
-    note.updatedAt = moment().valueOf()
-    // update the last edited text in ui
-    lastEditedDOM.textContent = generatedLastEditedText(note.updatedAt)
-    saveNotes(notes) // to local storage
-})
-
-// removing the note functionality
-removeButtonDOM.addEventListener('click', function(e) {
-    // remove the note from notes array
-    removeNote(notes, note.id)
-    saveNotes(notes) // to local storage
-    // redirect to home page
-    location.assign('/index.html')
-})
-
-// listem fpr changes in local storage
-window.addEventListener('storage', function(e) {
-    if((e.key === 'notes') || (localStorage.getItem('notes') === null)) { 
-        // act only if notes item is modified/deleted in localStorage
-        init()
-    } 
-})
