45514436bcecaa4c3e7c190f23cc4100102c7782
HC <hakan.cuzdan@yandex.com>
2018-10-19 14:41:50 +0300

    Section 9 : Lecture 85 : Catching and Throwing Errors

--- advanced/type-coercion.js
+++ advanced/type-coercion.js
@@ -1,4 +1,4 @@
-// Type coercion - a string, a number or a boolean
+// Type coercion - the result of type coercion will be a string, a number or a boolean
 console.log('5' + 5) // string '55'
 console.log('5' - 5) // number 0
 
@@ -8,10 +8,10 @@ console.log('5' == 5) // true, tries to coerce these two values into the same ty
 // === operator (strict equality operator) does take type into account
 console.log('5' === 5) // types do not match, returns false
 
-console.log(typeof 123)
-console.log(typeof {})
+console.log(typeof 123) // number
+console.log(typeof {})  // object
 
 const value = true + 12
 const type = typeof value
-console.log(type)
-console.log(value)
+console.log(type) // number
+console.log(value) // 13
--- functions/grade-calc.js
+++ functions/grade-calc.js
@@ -2,6 +2,14 @@
 // 15/20 -> You got a C (75%)!
 // A 90-100, B 80-89, C 70-79, D 60-69, F 0-59
 const gradeCalc = function(score, totalScore) {
+
+    if(typeof score !== 'number') {
+        throw Error('Argument "score" must be a number')
+    }
+    if(typeof totalScore !== 'number') {
+        throw Error('Argument "totalScore" must be a number')
+    }
+
     const percentage = (score / totalScore) * 100
     let grade = null
     if(percentage >= 90) {
@@ -18,15 +26,36 @@ const gradeCalc = function(score, totalScore) {
     return `You got a ${grade} (${percentage}%)!`
 }
 
-console.log(gradeCalc(15, 20))
-console.log(gradeCalc(100, 100))
-console.log(gradeCalc(90, 100))
-console.log(gradeCalc(80, 100))
-console.log(gradeCalc(70, 100))
-console.log(gradeCalc(60, 100))
-console.log(gradeCalc(50, 100))
-console.log(gradeCalc(40, 100))
-console.log(gradeCalc(30, 100))
-console.log(gradeCalc(20, 100))
-console.log(gradeCalc(10, 100))
-console.log(gradeCalc(0, 100))
+try {
+    // this block runs fully without throwing any error
+    console.log(gradeCalc(15, 20))
+    console.log(gradeCalc(100, 100))
+    console.log(gradeCalc(90, 100))
+    console.log(gradeCalc(80, 100))
+    console.log(gradeCalc(70, 100))
+    console.log(gradeCalc(60, 100))
+    console.log(gradeCalc(50, 100))
+    console.log(gradeCalc(40, 100))
+    console.log(gradeCalc(30, 100))
+    console.log(gradeCalc(20, 100))
+    console.log(gradeCalc(10, 100))
+    console.log(gradeCalc(0, 100))
+} catch(e) {
+    console.log('Catch block #1 is running')
+}
+
+try {
+    console.log(gradeCalc(true, 20))
+} catch(e) {
+    console.log('Catch block #2 is running') // this one runs
+    console.log(e.message)
+}
+
+try {
+    console.log(gradeCalc(15, true))
+} catch(e) {
+    console.log('Catch block #3 is running') // this one runs
+    console.log(e.message)    
+}
+
+console.log(gradeCalc(true, 20))
