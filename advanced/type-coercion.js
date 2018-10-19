e8347071c329d9496a24869f8e7049e242d6a9b4
HC <hakan.cuzdan@yandex.com>
2018-10-19 11:11:55 +0300

    Section 9 : Lecture 84 : Type Coercion

---
+++ advanced/type-coercion.js
@@ -0,0 +1,17 @@
+// Type coercion - a string, a number or a boolean
+console.log('5' + 5) // string '55'
+console.log('5' - 5) // number 0
+
+// == operator (loose equality operator) does not take type into account:
+// that means 
+console.log('5' == 5) // true, tries to coerce these two values into the same type
+// === operator (strict equality operator) does take type into account
+console.log('5' === 5) // types do not match, returns false
+
+console.log(typeof 123)
+console.log(typeof {})
+
+const value = true + 12
+const type = typeof value
+console.log(type)
+console.log(value)
