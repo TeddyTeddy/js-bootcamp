d19c1f27eaa64df5985dfe31fd0d294906f8147d
HC <hakan.cuzdan@yandex.com>
2018-10-23 13:59:21 +0300

    Section 10 : Lecture 99 : The Class Syntax : Applied to Person and Hangman

--- hangman/hangman.js
+++ hangman/hangman.js
@@ -1,74 +1,72 @@
-const Hangman = function(word, remainingGuesses) {
-    if((typeof word !== 'string') || (word === '')) {
-        throw Error('word must be a non-empty string')
-    }
-
-    if((typeof remainingGuesses !== 'number') || (remainingGuesses <= 0) ) {
-        throw Error('remainingGuesses must be a positive number')
-    }
-
-    // word and remainingGuesses arguments are valid..
-    this.word = word.toLowerCase().split('')
-    this.guessedLetters = []
-    this.remainingGuesses = remainingGuesses
-    this.status = 'playing' // one of the following values: "playing", "failed", "finished"
-}
-
-Hangman.prototype.getPuzzle = function() {
-    let puzzle = ''
-    this.word.forEach(letter => {
-        if(letter === ' ' || this.guessedLetters.includes(letter)) {
-            puzzle += letter
-        } else {
-            puzzle += '*'
+class Hangman {
+    constructor(word, remainingGuesses) {
+        if((typeof word !== 'string') || (word === '')) {
+            throw Error('word must be a non-empty string')
         }
-    })
-    return puzzle
-}
-
-Hangman.prototype.calculateStatus = function() {
-    if(this.remainingGuesses === 0) {
-        this.status = 'failed'
-    } else if( (!this.getPuzzle().includes('*')) && (this.remainingGuesses > 0)) {
-        this.status = 'finished'
-    } else {
-        this.status = 'playing'
-    }
-}
-
-Hangman.prototype.makeGuess = function(guess) {
-    // throw an error if guess is not a character
-    if((typeof guess !== 'string') || (guess.length !== 1)) {
-        throw Error('makeGuess methods expects a character')
-    }
-
-    // guess is a single character..
-    guess = guess.toLowerCase()
-    // if the letter is already guessed, do nothing
-    // if the status is 'failed' or 'finished' game is over, do nothing
-    if(this.guessedLetters.includes(guess) || this.status !== 'playing') {
-        return
-    }
-    // guess is unique; it has not been done before..
-    // & the game is not over, continue processing the guess
-    if(!this.word.includes(guess)) {
-        this.remainingGuesses--     // if the guess is incorrect, do reduce remainingGuesses by 1
-    }
     
-    this.guessedLetters.push(guess) // do this so that doGetPuzzle works properly
+        if((typeof remainingGuesses !== 'number') || (remainingGuesses <= 0) ) {
+            throw Error('remainingGuesses must be a positive number')
+        }
     
-    // guess is processed, calculate the status
-    this.calculateStatus()
-}
-
-Hangman.prototype.getStatusMessage= function() {
-    let statusText = ''
-    if(this.status === 'playing') {
-        statusText = `Guesses left: ${this.remainingGuesses}`
-    } else if(this.status === 'finished') {
-        statusText = 'Great work! You guessed the word'
-    } else if(this.status === 'failed') {
-        statusText = `Nice try! The word was: ${this.word.join('')}`
+        // word and remainingGuesses arguments are valid..
+        this.word = word.toLowerCase().split('')
+        this.guessedLetters = []
+        this.remainingGuesses = remainingGuesses
+        this.status = 'playing' // one of the following values: "playing", "failed", "finished"
     }
-    return statusText
+    getPuzzle() {
+        let puzzle = ''
+        this.word.forEach(letter => {
+            if(letter === ' ' || this.guessedLetters.includes(letter)) {
+                puzzle += letter
+            } else {
+                puzzle += '*'
+            }
+        })
+        return puzzle
+    }
+    calculateStatus() {
+        if(this.remainingGuesses === 0) {
+            this.status = 'failed'
+        } else if( (!this.getPuzzle().includes('*')) && (this.remainingGuesses > 0)) {
+            this.status = 'finished'
+        } else {
+            this.status = 'playing'
+        }
+    }
+    makeGuess(guess) {
+        // throw an error if guess is not a character
+        if((typeof guess !== 'string') || (guess.length !== 1)) {
+            throw Error('makeGuess methods expects a character')
+        }
+    
+        // guess is a single character..
+        guess = guess.toLowerCase()
+        // if the letter is already guessed, do nothing
+        // if the status is 'failed' or 'finished' game is over, do nothing
+        if(this.guessedLetters.includes(guess) || this.status !== 'playing') {
+            return
+        }
+        // guess is unique; it has not been done before..
+        // & the game is not over, continue processing the guess
+        if(!this.word.includes(guess)) {
+            this.remainingGuesses--     // if the guess is incorrect, do reduce remainingGuesses by 1
+        }
+        
+        this.guessedLetters.push(guess) // do this so that doGetPuzzle works properly
+        
+        // guess is processed, calculate the status
+        this.calculateStatus()
+    }
+    getStatusMessage() {
+        let statusText = ''
+        if(this.status === 'playing') {
+            statusText = `Guesses left: ${this.remainingGuesses}`
+        } else if(this.status === 'finished') {
+            statusText = 'Great work! You guessed the word'
+        } else if(this.status === 'failed') {
+            statusText = `Nice try! The word was: ${this.word.join('')}`
+        }
+        return statusText
+    }    
 }
--- oop/person.js
+++ oop/person.js
@@ -1,29 +1,23 @@
-
-const Person = function(firstName, lastName, age, likes = []) { // construction function
-    this.firstName = firstName
-    this.lastName = lastName
-    this.age = age
-    this.likes = likes
+class Person {
+    constructor(firstName, lastName, age, likes = []) {
+        this.firstName = firstName
+        this.lastName = lastName
+        this.age = age
+        this.likes = likes
+    }
+    getBio() {
+        let bio = `${this.firstName} is ${this.age}.`
+        this.likes.forEach((like) => { // arrow functions dont bind to THIS keyword
+            bio += ` ${this.firstName} likes ${like}.`
+        })
+        return bio        
+    }
+    setName(fullName) {
+        const names = fullName.split(' ')
+        this.firstName = names[0]
+        this.lastName = names[1]        
+    }
 }
-// Prototypal Inheritence
-// constructor function has its prototype property
-// Prototype is an object, on which we put everything we want to share
-// with the instances of the constructor function
-Person.prototype.getBio = function() {
-    let bio = `${this.firstName} is ${this.age}.`
-    this.likes.forEach((like) => { // arrow functions dont bind to THIS keyword
-        bio += ` ${this.firstName} likes ${like}.`
-    })
-    return bio
-}
-
-Person.prototype.setName = function(fullName) {
-    const names = fullName.split(' ')
-    this.firstName = names[0]
-    this.lastName = names[1]
-}
-
-Person.prototype.location = 'Thailand' // a shared property among Person instances
 
 const me = new Person('Andrew', 'Mead', 27, ['Biking', 'Teaching']) // returns a custom object type of person {}
 me.setName('Alexis Turner')
