e34a90841eda0d7b2e2bbd14d58bf1f1ed64d623
HC <hakan.cuzdan@yandex.com>
2018-10-26 17:35:33 +0300

    Section 10 : NetNinja : Prototypal Inheritance

---
+++ oop/prototypal-inheritance-2a.js
@@ -0,0 +1,40 @@
+class User {
+    constructor(email, name){
+        this.email = email;
+        this.name = name;
+        this.score = 0;
+    }
+    login(){
+        console.log(this.email, 'just logged in');
+        return this;
+    }
+    logout(){
+        console.log(this.email, 'just logged out');
+        return this;
+    }
+    updateScore(){
+        this.score++;
+        console.log(this.email, 'score is now', this.score);
+        return this;
+    }
+}
+
+class Admin extends User {
+    deleteUser(user){
+        users = users.filter(u => {
+            return u.email !== user.email
+        });
+    }
+}
+
+var userOne = new User('ryu@ninjas.com', 'Ryu');
+var userTwo = new User('yoshi@mariokorp.com', 'Yoshi');
+// myAdmin --> Admin.prototype --> User.Prototype --> Object.Prototype --> null
+var myAdmin = new Admin('shaun@ninjas.com', 'Shaun');
+// myAdmin.[[Prototype]] = Admin.prototype
+var users = [userOne, userTwo, myAdmin];
+
+myAdmin.deleteUser(userTwo);
+
+
+console.log(users);
