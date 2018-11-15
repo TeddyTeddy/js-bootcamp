7945c0f35d4188282a52bc352c5cff6b81b59db8
HC <hakan.cuzdan@yandex.com>
2018-11-15 11:37:33 +0200

    Todos App : Applying Module Pattern to todo-functions.js and using it in todo-app.js

--- todo-app/todo-app.js
+++ todo-app/todo-app.js
@@ -4,21 +4,21 @@ function runTodoApp() {
     const filters = {
         searchText: ''
     }
+
+    let todos = Engine.getSavedTodos() // from local storage
     
-    let todos = getSavedTodos() // from local storage
-    
-    renderTodos(todos, filters)
+    Engine.renderTodos(todos, filters)
     
     // filter todos
     document.querySelector('#filter-todos').addEventListener('input', e => {
         filters.searchText = e.target.value
-        renderTodos(todos, filters)
+        Engine.renderTodos(todos, filters)
     })
     
     // hide completed
     document.querySelector('#hide-completed').addEventListener('change', e => {
         filters.hideCompleted = e.target.checked
-        renderTodos(todos, filters)
+        Engine.renderTodos(todos, filters)
     })
     
     // add new todo
@@ -31,38 +31,38 @@ function runTodoApp() {
                 text: todoText,
                 completed: false
             })
-            saveTodos(todos) // to local storage
-            renderTodos(todos, filters)
+            Engine.saveTodos(todos) // to local storage
+            Engine.renderTodos(todos, filters)
             e.target.elements.newTodoText.value = '' // clear the input field newTodoText
         }
     })
     
-    // todo checkbox checked/unchecked : todo marked as done/undone
+    // todo checkbox checked/unchecked : todo marked as done/undone : utilizing the event bubling
     document.querySelector('#todos').addEventListener('change', e => {
         if(e.target.tagName === 'INPUT') { // act only if event is coming from checkbox
             const targetId = e.target.parentElement.getAttribute('id')
             const isChecked = e.target.checked
-            toggleTodo(todos, targetId, isChecked)
-            saveTodos(todos) // to local storage
-            renderTodos(todos, filters)
+            Engine.toggleTodo(todos, targetId, isChecked)
+            Engine.saveTodos(todos) // to local storage
+            Engine.renderTodos(todos, filters)
         }
     })
     
-    // removing a todo functionality
+    // removing a todo functionality utilizing event bubling
     document.querySelector('#todos').addEventListener('click', e => {
         if(e.target.tagName === 'BUTTON') { // act only if the click event is coming from the button itself
             const targetId = e.target.parentElement.getAttribute('id')
-            removeTodo(todos, targetId)
-            saveTodos(todos) // to local storage
-            renderTodos(todos, filters)
+            Engine.removeTodo(todos, targetId)
+            Engine.saveTodos(todos) // to local storage
+            Engine.renderTodos(todos, filters)
         }
     })
     
     // local storage changed event
     window.addEventListener('storage', e => {
         if((e.key === 'todos') || (!localStorage.getItem('todos'))) { // act only if todos item is changed in local storage
-            todos = getSavedTodos()
-            renderTodos(todos, filters)
+            todos = Engine.getSavedTodos()
+            Engine.renderTodos(todos, filters)
         }
     })
 }
--- todo-app/todo-functions.js
+++ todo-app/todo-functions.js
@@ -1,92 +1,102 @@
-// return todos from local storage if exist, if not, return an empty array
-const getSavedTodos = () => {
-    const todosJSON = localStorage.getItem('todos')
-    try{
-        return todosJSON ? JSON.parse(todosJSON) : []
-    } catch(e) {
-        // JSON.parse has thrown an exception because todos item is corrupted and 
-        // not in JSON format anymore
-        localStorage.setItem('todos', JSON.stringify([])) // fix the corrupted todos item in local storage
-        return []
-    }
-}
+const Engine = (function todoFunctions () {
 
-// save todos to local storage
-const saveTodos = todos => localStorage.setItem('todos', JSON.stringify(todos))
-
-
-const generateTodoDOM = todo => {
-    const divDOM = document.createElement('div')
-    divDOM.setAttribute('id', todo.id)
-
-    const checkboxDOM = document.createElement('input')
-    checkboxDOM.setAttribute('type', 'checkbox')
-    checkboxDOM.checked = todo.completed
-    divDOM.appendChild(checkboxDOM)
-
-    const spanDOM = document.createElement('span')
-    spanDOM.textContent = todo.text
-    divDOM.appendChild(spanDOM)
-
-    const removeBtnDOM = document.createElement('button')
-    removeBtnDOM.textContent = 'x'
-    divDOM.appendChild(removeBtnDOM)
-
-    return divDOM
-}
-
-const getIncompleTodosCount = todos => {
-    let count = 0
-    todos.forEach(todo => {
-        if(!todo.completed) {
-            ++count
+    // return todos from local storage if exist, if not, return an empty array
+    const getSavedTodos = () => {
+        const todosJSON = localStorage.getItem('todos')
+        try{
+            return todosJSON ? JSON.parse(todosJSON) : []
+        } catch(e) {
+            // JSON.parse has thrown an exception because todos item is corrupted and 
+            // not in JSON format anymore
+            localStorage.setItem('todos', JSON.stringify([])) // fix the corrupted todos item in local storage
+            return []
         }
-    })
-    return count
-}
-
-const filterTodos = (todos, filters) => {
-    return todos.filter( todo => {
-        const includesSearchText = todo.text.toLowerCase().includes(filters.searchText.toLowerCase())
-        const hideTodo = filters.hideCompleted && todo.completed
-        return includesSearchText && !hideTodo
-    })
-}
-
-const renderTodos = (todos, filters) => {
-    const filteredTodos = filterTodos(todos, filters)
-
-    const todosDOM = document.querySelector('#todos')
-    todosDOM.innerHTML = ''
-
-    // render 'You have x todos left' headline
-    const h1DOM = document.createElement('h1')
-    const count = getIncompleTodosCount(filteredTodos)
-    h1DOM.textContent = `You have ${count} todo(s) left`
-    todosDOM.appendChild(h1DOM)
-
-    filteredTodos.forEach( todo => {
-        const todoDOM = generateTodoDOM(todo)
-        todosDOM.appendChild(todoDOM)
-    })
-}
-
-// toggleTodo with target id with isChecked
-const toggleTodo = (todos, targetId, isChecked) => {
-    const todo = todos.find(todo => todo.id === targetId)
-    if(todo) {
-        todo.completed = isChecked
-    } else {
-        console.log(`Warning: todo with id ${targetId} is not found in todos array`)
     }
-}
 
-// removeTodo with targetId from todos array
-const removeTodo = (todos, targetId) => {
-    const index = todos.findIndex(todo => todo.id === targetId )
-    if(index === -1) {
-        console.log(`Warning: todo with id ${targetId} not found in todos array`)
-    } else {
-        todos.splice(index, 1) // remove the very todo which has a matching id from notes array
+    // save todos to local storage
+    const saveTodos = todos => localStorage.setItem('todos', JSON.stringify(todos))
+
+    const _generateTodoDOM = todo => {
+        const divDOM = document.createElement('div')
+        divDOM.setAttribute('id', todo.id)
+
+        const checkboxDOM = document.createElement('input')
+        checkboxDOM.setAttribute('type', 'checkbox')
+        checkboxDOM.checked = todo.completed
+        divDOM.appendChild(checkboxDOM)
+
+        const spanDOM = document.createElement('span')
+        spanDOM.textContent = todo.text
+        divDOM.appendChild(spanDOM)
+
+        const removeBtnDOM = document.createElement('button')
+        removeBtnDOM.textContent = 'x'
+        divDOM.appendChild(removeBtnDOM)
+
+        return divDOM
     }
-}
+
+    const _getIncompleTodosCount = todos => {
+        let count = 0
+        todos.forEach(todo => {
+            if(!todo.completed) {
+                ++count
+            }
+        })
+        return count
+    }
+
+    const _filterTodos = (todos, filters) => {
+        return todos.filter( todo => {
+            const includesSearchText = todo.text.toLowerCase().includes(filters.searchText.toLowerCase())
+            const hideTodo = filters.hideCompleted && todo.completed
+            return includesSearchText && !hideTodo
+        })
+    }
+
+    const renderTodos = (todos, filters) => {
+        const filteredTodos = _filterTodos(todos, filters)
+
+        const todosDOM = document.querySelector('#todos')
+        todosDOM.innerHTML = ''
+
+        // render 'You have x todos left' headline
+        const h1DOM = document.createElement('h1')
+        const count = _getIncompleTodosCount(filteredTodos)
+        h1DOM.textContent = `You have ${count} todo(s) left`
+        todosDOM.appendChild(h1DOM)
+
+        filteredTodos.forEach( todo => {
+            const todoDOM = _generateTodoDOM(todo)
+            todosDOM.appendChild(todoDOM)
+        })
+    }
+
+    // toggleTodo with target id with isChecked
+    const toggleTodo = (todos, targetId, isChecked) => {
+        const todo = todos.find(todo => todo.id === targetId)
+        if(todo) {
+            todo.completed = isChecked
+        } else {
+            console.log(`Warning: todo with id ${targetId} is not found in todos array`)
+        }
+    }
+
+    // removeTodo with targetId from todos array
+    const removeTodo = (todos, targetId) => {
+        const index = todos.findIndex(todo => todo.id === targetId )
+        if(index === -1) {
+            console.log(`Warning: todo with id ${targetId} not found in todos array`)
+        } else {
+            todos.splice(index, 1) // remove the very todo which has a matching id from notes array
+        }
+    }
+
+    return {
+        getSavedTodos: getSavedTodos,
+        saveTodos: saveTodos,
+        renderTodos: renderTodos,
+        toggleTodo: toggleTodo,
+        removeTodo: removeTodo
+    }
+})()
